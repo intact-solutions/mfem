4e5106640a238c0cff2629269ee873ad345b1c12
G Kumar <gkumar@intact-solutions.com>
2020-11-10 09:34:22 -0600

    1D heat transfer problem working. Omega function implemented too

--- examples/ex1.cpp
+++ examples/ex1.cpp
@@ -57,38 +57,150 @@
 using namespace std;
 using namespace mfem;
 
+// Initial condition
+void omega_function(const Vector& x, Vector &y) {
+  y.SetSize(2);
+  y[0] = x[0];
+  y[1] = 1; //derivitive of x
+};
+
+
+/** Class for integrating the bilinear form a(u,v) := (Q grad u, grad v) where Q
+    can be a scalar or a matrix coefficient. */
+class DiffusionIntegrator_Omega : public BilinearFormIntegrator
+{
+protected:
+  Coefficient* Q;
+  MatrixCoefficient* MQ;
+  int intorder = 1;
+  VectorFunctionCoefficient* omega;
+
+private:
+  Vector vec, pointflux, shape;
+#ifndef MFEM_THREAD_SAFE
+  DenseMatrix dshape, dshapedxt, invdfdx, mq;
+  DenseMatrix te_dshape, te_dshapedxt;
+#endif
+
+  // PA extension
+  const FiniteElementSpace* fespace;
+  const DofToQuad* maps;         ///< Not owned
+  const GeometricFactors* geom;  ///< Not owned
+  int dim, ne, dofs1D, quad1D;
+  Vector pa_data;
+
+#ifdef MFEM_USE_CEED
+  // CEED extension
+  CeedData* ceedDataPtr;
+#endif
+
+public:
+  /// Construct a diffusion integrator with coefficient Q = 1
+  DiffusionIntegrator_Omega()
+  {
+    Q = NULL;
+    MQ = NULL;
+    maps = NULL;
+    geom = NULL;
+    omega = NULL;
+#ifdef MFEM_USE_CEED
+    ceedDataPtr = NULL;
+#endif
+  }
+
+  /// Construct a diffusion integrator with a scalar coefficient q
+  DiffusionIntegrator_Omega(Coefficient& q, int order, VectorFunctionCoefficient& omega_in)
+    : Q(&q), intorder(order), omega(&omega_in)
+  {
+    MQ = NULL;
+    maps = NULL;
+    geom = NULL;
+#ifdef MFEM_USE_CEED
+    ceedDataPtr = NULL;
+#endif
+  }
+
+ /* /// Construct a diffusion integrator with a matrix coefficient q
+  DiffusionIntegrator_Omega(MatrixCoefficient& q, int order, FunctionCoefficient omega_in)
+    : MQ(&q), intorder(order), omega(omega_in)
+  {
+    Q = NULL;
+    maps = NULL;
+    geom = NULL;
+#ifdef MFEM_USE_CEED
+    ceedDataPtr = NULL;
+#endif
+  }*/
+
+  virtual ~DiffusionIntegrator_Omega()
+  {
+#ifdef MFEM_USE_CEED
+    delete ceedDataPtr;
+#endif
+  }
+
+  /** Given a particular Finite Element
+      computes the element stiffness matrix elmat. */
+  virtual void AssembleElementMatrix(const FiniteElement& el,
+    ElementTransformation& Trans,
+    DenseMatrix& elmat) {
+    int nd = el.GetDof();
+    int dim = el.GetDim();
+    int spaceDim = Trans.GetSpaceDim();
+    bool square = (dim == spaceDim);
+    double w;
+
+    dshape.SetSize(nd, dim);
+    dshapedxt.SetSize(nd, spaceDim);
+    invdfdx.SetSize(dim, spaceDim);
+
+    elmat.SetSize(nd);
+
+    const IntegrationRule ir = IntRules.Get(el.GetGeomType(), intorder);
+
+    elmat = 0.0;
+    for (int i = 0; i < ir.GetNPoints(); i++)
+    {
+      const IntegrationPoint& ip = ir.IntPoint(i);
+      el.CalcDShape(ip, dshape);
+
+      Trans.SetIntPoint(&ip);
+      w = Trans.Weight();
+      w = ip.weight / (square ? w : w * w * w);
+      
+      Vector d_dx;
+      omega->Eval(d_dx, Trans, ip);
+      
+      // AdjugateJacobian = / adj(J),         if J is square
+      //                    \ adj(J^t.J).J^t, otherwise
+      Mult(dshape, Trans.AdjugateJacobian(), dshapedxt);
+      if (!MQ)
+      {
+        if (Q)
+        {
+          w *= Q->Eval(Trans, ip);
+        }
+        AddMult_a_AAt(w, dshapedxt, elmat);
+      }
+      else
+      {
+        MQ->Eval(invdfdx, Trans, ip);
+        invdfdx *= w;
+        Mult(dshapedxt, invdfdx, dshape);
+        AddMultABt(dshape, dshapedxt, elmat);
+      }
+    }
+  };  
+};
+
 int main(int argc, char *argv[])
 {
-   // 1. Parse command-line options.
-   const char *mesh_file = "../data/star.mesh";
+   // 1. Parse command-line options.   
    int order = 1;
    bool static_cond = false;
    bool pa = false;
    const char *device_config = "cpu";
    bool visualization = true;
-
-   OptionsParser args(argc, argv);
-   args.AddOption(&mesh_file, "-m", "--mesh",
-                  "Mesh file to use.");
-   args.AddOption(&order, "-o", "--order",
-                  "Finite element order (polynomial degree) or -1 for"
-                  " isoparametric space.");
-   args.AddOption(&static_cond, "-sc", "--static-condensation", "-no-sc",
-                  "--no-static-condensation", "Enable static condensation.");
-   args.AddOption(&pa, "-pa", "--partial-assembly", "-no-pa",
-                  "--no-partial-assembly", "Enable Partial Assembly.");
-   args.AddOption(&device_config, "-d", "--device",
-                  "Device configuration string, see Device::Configure().");
-   args.AddOption(&visualization, "-vis", "--visualization", "-no-vis",
-                  "--no-visualization",
-                  "Enable or disable GLVis visualization.");
-   args.Parse();
-   if (!args.Good())
-   {
-      args.PrintUsage(cout);
-      return 1;
-   }
-   args.PrintOptions(cout);
 
    // 2. Enable hardware devices such as GPUs, and programming models such as
    //    CUDA, OCCA, RAJA and OpenMP based on command line options.
@@ -98,63 +210,40 @@ int main(int argc, char *argv[])
    // 3. Read the mesh from the given mesh file. We can handle triangular,
    //    quadrilateral, tetrahedral, hexahedral, surface and volume meshes with
    //    the same code.
-   Mesh *mesh = new Mesh(mesh_file, 1, 1);
-   int dim = mesh->Dimension();
-
-   // 4. Refine the mesh to increase the resolution. In this example we do
-   //    'ref_levels' of uniform refinement. We choose 'ref_levels' to be the
-   //    largest number that gives a final mesh with no more than 50,000
-   //    elements.
-   {
-      int ref_levels =
-         (int)floor(log(50000./mesh->GetNE())/log(2.)/dim);
-      for (int l = 0; l < ref_levels; l++)
-      {
-         mesh->UniformRefinement();
-      }
-   }
+   Mesh *mesh = new Mesh(10,2.0);
+   int dim = mesh->Dimension();  
 
    // 5. Define a finite element space on the mesh. Here we use continuous
    //    Lagrange finite elements of the specified order. If order < 1, we
    //    instead use an isoparametric/isogeometric space.
    FiniteElementCollection *fec;
-   if (order > 0)
-   {
-      fec = new H1_FECollection(order, dim);
-   }
-   else if (mesh->GetNodes())
-   {
-      fec = mesh->GetNodes()->OwnFEC();
-      cout << "Using isoparametric FEs: " << fec->Name() << endl;
-   }
-   else
-   {
-      fec = new H1_FECollection(order = 1, dim);
-   }
+   fec = new H1_FECollection(order = 1, dim);
+
    FiniteElementSpace *fespace = new FiniteElementSpace(mesh, fec);
    cout << "Number of finite element unknowns: "
         << fespace->GetTrueVSize() << endl;
+   int size = fespace->GetVSize();
 
    // 6. Determine the list of true (i.e. conforming) essential boundary dofs.
-   //    In this example, the boundary conditions are defined by marking all
-   //    the boundary attributes from the mesh as essential (Dirichlet) and
-   //    converting them to a list of true dofs.
-   Array<int> ess_tdof_list;
-   if (mesh->bdr_attributes.Size())
-   {
-      Array<int> ess_bdr(mesh->bdr_attributes.Max());
-      ess_bdr = 1;
-      fespace->GetEssentialTrueDofs(ess_bdr, ess_tdof_list);
-   }
+   //    In this example, the boundary conditions are defined by marking only
+   //    boundary attribute 1 from the mesh as essential and converting it to a
+   //    list of true dofs.
+   Array<int> ess_tdof_list, ess_bdr(mesh->bdr_attributes.Max());
+   ess_bdr = 0;
+   ess_bdr[0] = 1;
+   fespace->GetEssentialTrueDofs(ess_bdr, ess_tdof_list);
+   cout << "\nessential dofs: ";
+   ess_tdof_list.Print();
 
-   // 7. Set up the linear form b(.) which corresponds to the right-hand side of
-   //    the FEM linear system, which in this case is (1,phi_i) where phi_i are
-   //    the basis functions in the finite element fespace.
-   LinearForm *b = new LinearForm(fespace);
+  
+   Vector rhs(size);
+   rhs = 0.0;   
+   rhs[size-1] = 5.0;
+   cout << "\nRHS: ";
+   rhs.Print();
+   
    ConstantCoefficient one(1.0);
-   b->AddDomainIntegrator(new DomainLFIntegrator(one));
-   b->Assemble();
-
+   
    // 8. Define the solution vector x as a finite element grid function
    //    corresponding to fespace. Initialize x with initial guess of zero,
    //    which satisfies the boundary conditions.
@@ -164,9 +253,11 @@ int main(int argc, char *argv[])
    // 9. Set up the bilinear form a(.,.) on the finite element space
    //    corresponding to the Laplacian operator -Delta, by adding the Diffusion
    //    domain integrator.
+   VectorFunctionCoefficient omega(2, omega_function);
+
    BilinearForm *a = new BilinearForm(fespace);
    if (pa) { a->SetAssemblyLevel(AssemblyLevel::PARTIAL); }
-   a->AddDomainIntegrator(new DiffusionIntegrator(one));
+   a->AddDomainIntegrator(new DiffusionIntegrator_Omega(one, 2, omega));
 
    // 10. Assemble the bilinear form and the corresponding linear system,
    //     applying any necessary transformations such as: eliminating boundary
@@ -177,7 +268,7 @@ int main(int argc, char *argv[])
 
    OperatorPtr A;
    Vector B, X;
-   a->FormLinearSystem(ess_tdof_list, x, *b, A, X, B);
+   a->FormLinearSystem(ess_tdof_list, x, rhs, A, X, B);
 
    cout << "Size of linear system: " << A->Height() << endl;
 
@@ -203,30 +294,21 @@ int main(int argc, char *argv[])
    }
 
    // 12. Recover the solution as a finite element grid function.
-   a->RecoverFEMSolution(X, *b, x);
+   a->RecoverFEMSolution(X, rhs, x);
+   //cout << "\nSolution: "; x.Print();
 
-   // 13. Save the refined mesh and the solution. This output can be viewed later
-   //     using GLVis: "glvis -m refined.mesh -g sol.gf".
-   ofstream mesh_ofs("refined.mesh");
-   mesh_ofs.precision(8);
-   mesh->Print(mesh_ofs);
-   ofstream sol_ofs("sol.gf");
-   sol_ofs.precision(8);
-   x.Save(sol_ofs);
+   GridFunction grad_x(fespace);
+   grad_x = 0.0;
+   x.GetDerivative(1, 0, grad_x);
+   //cout << "\nGrad X"; grad_x.Print();
 
-   // 14. Send the solution by socket to a GLVis server.
-   if (visualization)
-   {
-      char vishost[] = "localhost";
-      int  visport   = 19916;
-      socketstream sol_sock(vishost, visport);
-      sol_sock.precision(8);
-      sol_sock << "solution\n" << *mesh << x << flush;
-   }
-
+   ofstream vtk_ofs("D:\\OneDrive\\Documents\\VisualStudio2017\\Projects\\mfem\\examples\\ex1_sol.vtk");
+   mesh->PrintVTK(vtk_ofs, 1, 0);
+   x.SaveVTK(vtk_ofs, "sol", 1);
+   grad_x.SaveVTK(vtk_ofs, "grad_x", 1);
+      
    // 15. Free the used memory.
-   delete a;
-   delete b;
+   delete a;   
    delete fespace;
    if (order > 0) { delete fec; }
    delete mesh;
